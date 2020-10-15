e987259e353b4b2d7b599995c1517254885c90fa
sith1969 <sith@tut.by>
2020-10-15 11:53:10 +0300

    fiat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
@@ -39,8 +40,38 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let sym = [];
+    let result = [];
+    let [i,j] = [0,1];
+    while (i < expr.length) {
+        let simvol ='';
+        sym =(expr.slice(i,(+(j+'0'))));
+        for (let i = 0; i <= 10; i = i + 2) {
+            if(sym[i] === '*') {
+                simvol = ` `;
+                break;
+            }
+            if(sym[i] + sym[i+1] === '00') {
+                simvol += '';
+                continue;
+            }
+            if(sym[i] + sym[i+1] === '10') {
+                simvol += '.';
+                continue;
+            }
+            if(sym[i] + sym[i+1] === '11') {
+                simvol += '-';
+                continue;
+            }
+        }
+        if (simvol === ` `) {result.push(` `);}
+        else {result.push(MORSE_TABLE[simvol]);}
+        i = i+10;
+        j++;
+    }
+    return result.join('');
 }
 
 module.exports = {
     decode
-}
+};
